652fadd500ca992ee83e24b77daac4a967601f78
Gilbert Guszcza <guszczagilbert@gmail.com>
2024-05-16 00:24:08 +0200

    _activation_functions

--- layer.py
+++ layer.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy
 import numpy as np
 from functools import reduce
@@ -5,7 +7,18 @@ import random
 
 
 class Layer:
-    _activation_functions_names = ["linear", "relu", "gelu"]
+    def __relu(x):
+        return np.maximum(x, 0)
+
+    def __linear(x):
+        return x
+
+    def __gelu(x):
+        return 0.5 * x * (1 + np.tanh(math.sqrt(2 / np.pi) * (x + 0.044715 * np.power(x, 3))))
+
+    _activation_functions = {"linear" : __linear,
+                             "relu" : __relu,
+                             "gelu" : __gelu}
 
     def __init__(self,**kwargs):
         if kwargs["neurons_count"] is None: raise Exception("Require to receive amount of neurons")
@@ -14,8 +27,8 @@ class Layer:
         self.input_shape = set(kwargs["input_shape"])
 
         if kwargs["activation"] is None: raise Exception("Require to receive activation function")
-        elif kwargs["activation"] not in Layer._activation_functions_names: raise Exception(f"Doesn't recognize \"{kwargs['activation']}\" as a activation function")
-        else: self.activation = kwargs["activation"]
+        elif kwargs["activation"] not in Layer._activation_functions.keys(): raise Exception(f"Doesn't recognize \"{kwargs['activation']}\" as a activation function")
+        else: self.activation = Layer._activation_functions[kwargs["activation"]]
 
         self.weights = None
 
@@ -25,12 +38,11 @@ class Layer:
         elif self.input_shape is None: self.input_shape = set(kwargs["input_shape"])
         self.weights = np.random.normal(size=(self.neurons_count, reduce(lambda x,y: x*y,self.input_shape)))
 
-    #Returns weights * X
+    #Returns activation_function(weights * X)
     def calc(self,X:numpy.array):
         if set(X.shape) != self.input_shape: raise Exception("Incorrect X's shape")
 
         if len(X.shape) != 1: X = X.reshape((self.weights.shape[1]))
 
-        return np.matmul(self.weights, X)
-
+        return self.activation(np.matmul(self.weights, X))
 
